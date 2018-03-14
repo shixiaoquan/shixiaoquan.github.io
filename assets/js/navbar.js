1b3f0de2896366f855a794b46307e8728fa29599
shixiaoquan <1010083274@qq.com>
2018-03-14 12:07:28 +0800

    modify nav bar style

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -12,6 +12,32 @@ $(function () {
 });
 
 /*
+ * 用于控制菜单栏的选中情况
+ */
+var urlstr = location.href;
+var urlstatus=false;
+$("#nav li a").each(function () {
+    var _urlstr = urlstr.replace(/\//g, '')
+    var _href = $(this).attr('href').replace(/\//g, '')
+    if(_href.indexOf('http') === -1) {
+        _href = location.origin.replace(/\//g, '') + _href
+    }
+
+    // console.log('urlstr:', _urlstr)
+    // console.log('href:', _href)
+
+    if (_urlstr === _href && $(this).attr('href')!='') {
+      // console.log($(this).attr('href'))
+      $(this).parent().addClass('selectedli');
+      urlstatus = true;
+    } else {
+      $(this).parent().removeClass('selectedli');
+    }
+});
+
+// if (!urlstatus) {$("#nav li").eq(3).addClass('selectedli'); }
+
+/*
  * Display the navbar back to normal after resize
  */
 $(window).resize(function () {
